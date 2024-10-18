4a2de933b0ceb10484f3218f071285a9ef933087
hannixminji <hannixminji04@gmail.com>
2024-10-18 14:31:56 +0800

    Minor changes

--- departments/DepartmentValidator.php
+++ departments/DepartmentValidator.php
@@ -2,7 +2,19 @@
 
 class DepartmentValidator
 {
+    private Department $department;
+
     public function __construct(Department $department)
+    {
+        $this->department = $department;
+    }
+
+    public function validate(): string
+    {
+        return '';
+    }
+
+    public function isValidName(string $name)
     {
         //
     }
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 session_start();
 
+// Kunwari eto muna yung user na nag logged in
 $_SESSION['user_logged_in'] = false;
 $_SESSION['user_access_role'] = 'admin';
 $_SESSION['user_last_activity'] = time();
